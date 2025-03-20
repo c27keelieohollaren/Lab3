9d9cfe66ae6b53c0e72f3b6ee8ad06885cf71889
c27keelieohollaren <c27keelie.ohollaren@afacademy.af.edu>
2025-03-20 10:54:17 -0600

    Update thunderbird_fsm.vhd

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -36,18 +36,18 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 One Hot State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 1,0,0,0,0,0,0,0
+--|                  ON    | 0,1,0,0,0,0,0,0
+--|                  R1    | 0,0,1,0,0,0,0,0    
+--|                  R2    | 0,0,0,1,0,0,0,0
+--|                  R3    | 0,0,0,0,1,0,0,0    
+--|                  L1    | 0,0,0,0,0,1,0,0
+--|                  L2    | 0,0,0,0,0,0,1,0
+--|                  L3    | 0,0,0,0,0,0,0,1
 --|                 --------------------
 --|
 --|
@@ -85,16 +85,23 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
  
-entity thunderbird_fsm is 
---  port(
-	
---  );
+entity thunderbird_fsm is
+    port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
 -- CONSTANTS ------------------------------------------------------------------
-  
+  type state_type is (
+        S_OFF, S_ON, S_R1, S_R2, S_R3, S_L1, S_L2, S_L3
+    );
+  signal f_state, f_next_state : state_type;
+  signal state_reg : std_logic_vector(7 downto 0);
 begin
 
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
@@ -102,7 +109,90 @@ begin
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
+     process (i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            f_state <= S_OFF;  -- Reset to OFF state
+        elsif rising_edge(i_clk) then
+            f_state <= f_next_state;
+        end if;
+    end process;
     
+    
+    process (f_state, i_left, i_right)
+    begin
+        case f_state is
+            when S_OFF =>
+                if i_left = '1' and i_right = '0' then
+                    f_next_state <= S_L1;
+                elsif i_right = '1' and i_left = '0' then
+                    f_next_state <= S_R1;
+                elsif i_left = '1' and i_right = '1' then
+                    f_next_state <= S_ON;  -- Hazard mode
+                else
+                    f_next_state <= S_OFF;
+                end if;
+            
+            when S_ON =>
+                f_next_state <= S_OFF;  -- Hazard mode toggles back to OFF
+            
+            when S_R1 =>
+                f_next_state <= S_R2;
+            when S_R2 =>
+                f_next_state <= S_R3;
+            when S_R3 =>
+                f_next_state <= S_OFF;
+            
+            when S_L1 =>
+                f_next_state <= S_L2;
+            when S_L2 =>
+                f_next_state <= S_L3;
+            when S_L3 =>
+                f_next_state <= S_OFF;
+            
+            when others =>
+                f_next_state <= S_OFF;
+        end case;
+    end process;
+
+
+process (f_state)
+    begin
+        -- Default outputs
+        o_lights_L <= "000";
+        o_lights_R <= "000";
+        state_reg  <= "10000000";
+        
+        case f_state is
+            when S_ON =>
+                o_lights_L <= "111";
+                o_lights_R <= "111";
+                state_reg  <= "01000000";
+            when S_R1 =>
+                o_lights_R <= "001";
+                state_reg  <= "00100000";
+            when S_R2 =>
+                o_lights_R <= "011";
+                state_reg  <= "00010000";
+            when S_R3 =>
+                o_lights_R <= "111";
+                state_reg  <= "00001000";
+            when S_L1 =>
+                o_lights_L <= "001";
+                state_reg  <= "00000100";
+            when S_L2 =>
+                o_lights_L <= "011";
+                state_reg  <= "00000010";
+            when S_L3 =>
+                o_lights_L <= "111";
+                state_reg  <= "00000001";
+            when others =>
+                o_lights_L <= "000";
+                o_lights_R <= "000";
+                state_reg  <= "10000000";
+        end case;
+    end process;
+
 	-----------------------------------------------------					   
 				  
 end thunderbird_fsm_arch;
